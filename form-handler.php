d8bf03e6735f9cb036f231fdcc334d2faa5533e3
AdityaGaikar91 <gaikaditya795@gmail.com>
2023-03-25 00:13:05 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = '';
+$email_form = 'contact@aisadept.in';
 
 $email_subject = 'New Form Submission';
 
